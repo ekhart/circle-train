49272e9d0c74f5f1bc83f2cfeb7168139d2b962d
Artur Dębkowski <arturdebkowski@gmail.com>
2017-03-20 11:08:46 +0100

    add update hook to player - print msg

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -4,9 +4,14 @@
 		arcadia.linear
 		hard.core))
 
+(defn handle-input [o]
+	(log "handle-input"))
+
 (defn start-game [o]
 	(clear-cloned!)
 	(clone! :camera)
-	(clone! :sun))
+	(clone! :sun)
+	(let [player (clone! :player)]
+		(hook+ player :update #'game.core/handle-input)))
 
 (start-game nil)
