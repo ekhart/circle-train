07af91b84eebd71eba9057ac6ad7ceb4bf80b957
Artur Dębkowski <arturdebkowski@gmail.com>
2017-03-20 11:15:33 +0100

    player input

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -5,9 +5,11 @@
 		hard.core
 		hard.input))
 
+(def pspeed 6)
 
 (defn handle-input [o]
-	(if (key? "a") (log "left")))
+	(if (key? "a") (rotate! o (v3 0 (- pspeed) 0)))
+	(if (key? "d") (rotate! o (v3 0 pspeed 0))))
 
 (defn start-game [o]
 	(clear-cloned!)
