41e2887bf89f34b7748614e767240fa77830591a
Artur Dębkowski <arturdebkowski@gmail.com>
2017-03-23 11:24:34 +0100

    fixed segment creation

--- Assets/game/core.clj
+++ Assets/game/core.clj
@@ -12,22 +12,34 @@
 	(if (key? "d") (rotate! o (v3 0 pspeed 0))))
 
 (defn make-ring [pattern]
-	(map-indexed 
-		(fn [i s]
-			(if (= s 1)
-				(rotate! (clone! :segment)
-					(v3 0 (* i 60) 0))))
-		pattern))
+	(dorun
+		(map-indexed 
+			(fn [i s]
+				(if (= s 1)
+					(rotate! (clone! :segment)
+						(v3 0 (* i 60) 0))))
+			pattern)))
 
 (defn start-game [o]
 	(clear-cloned!)
 	(clone! :camera)
 	(clone! :sun)
 	(let [player (clone! :player)]
-		(hook+ player :update #'game.core/handle-input)))
+		(hook+ player :update #'game.core/handle-input))
+	(make-ring [1 0 1 0 1 0]))
 
 (start-game nil)
 
-(clone! :segment)
 
-(make-ring [1 0 1 0 1 0])
+; (clone! :segment)
+
+; (rotate! (clone! :segment) (v3 0 (* 0 60) 0))
+; (rotate! (clone! :segment) (v3 0 (* 1 60) 0))
+; (log "hej")
+; (log [1 0 1 0 1 0])
+; (log (str 0 " " 1))
+; (log map-indexed)
+; ; map-indexed is Lazy - dorun?
+; (log (map-indexed #(log (str %1 " " %2)) [1 0 1 0 1 0]))
+; ; now its working!
+; (dorun (map-indexed #(log (str %1 " " %2)) [1 0 1 0 1 0]))
